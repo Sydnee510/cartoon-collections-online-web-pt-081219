82758b4fbfb4c0af95c3884e4495042f84169b9c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 00:59:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,11 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  
+   if array.any? { |item| item.length > 4 }
+    return true 
+  else 
+    return false
+  end  
 end
 
 def find_the_cheese# code an argument here
