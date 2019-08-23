85e12bc44d99e940337e36f80065a08e84ac845f
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 00:59:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,14 @@
 def roll_call_dwarves(array)
   array.each_with_index { |item, index|
  puts "#{index +1}, #{item}" }
-  # Your code here
 end
 
 def summon_captain_planet(array) 
    array.map { |item| item.capitalize << "!"}
-  # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
